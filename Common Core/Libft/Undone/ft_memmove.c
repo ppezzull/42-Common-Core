0e10b29dafeadd2c7eeff672be87420533521c92
Pietro Jairo Pezzullo <ppezzull@e3r12p4.42roma.it>
2023-01-17 21:30:45 +0100

    Sleepy push

--- Common Core/Libft/Undone/ft_memmove.c
+++ Common Core/Libft/Undone/ft_memmove.c
@@ -1,26 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ppezzull <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/17 18:29:20 by ppezzull          #+#    #+#             */
+/*   Updated: 2023/01/17 18:29:22 by ppezzull         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
-void *ft_memmove(void *str1, const void *str2, int n);
+void	*ft_memmove(void *str1, const void *str2, int n);
 
-int main (int argc, char** argv)
+int	main(int argc, char	**argv)
 {
-   	char	*src;
+	char	*src;
 	char	*dest;
 	char	*dest2;
 	int		n;
 
-	if(argc == 4)
-	{
-   		dest = argv[1];
-   		dest2 = argv[1];
-   		src = argv[2];
-		n = atoi(argv[3]);
-   		printf("Before memcpy dest = %s\n", dest);
-   		memmove(dest, src, n);
-   		printf("memmove dest = %s\n", dest);
-		// ft_memmove(dest2, src, n);
-   		// printf("ft_memmove dest = %s\n", dest2);
-	}
+	dest = argv[1];
+	dest2 = argv[1];
+	n = atoi(argv[2]);
+	printf("Before memcpy dest = %s\n", dest);
+	memmove(dest + 3, dest, n);
+	printf("memmove dest = %s\n", dest);
+	// ft_memmove(dest2, src, n);
+	// printf("ft_memmove dest = %s\n", dest2);
 }
