e709833c21f14a82abeb8cd1b67162a70caa0a0d
Pietro Jairo Pezzullo <ppezzull@e3r7p8.42roma.it>
2023-02-10 12:05:55 +0100

    nextline valuation 112 ;-(

---
+++ Common Core/getNextLine/get_next_line_bonus.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pietro <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/04 14:24:37 by pietro            #+#    #+#             */
+/*   Updated: 2023/02/04 14:24:39 by pietro           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_line(int fd, char *string)
+{
+	char	*buffer;
+	size_t	bytes;
+
+	buffer = (char *) malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	bytes = 1;
+	while (bytes > 0 && ft_strchr(string, '\n') == 0)
+	{
+		bytes = read(fd, buffer, BUFFER_SIZE);
+		if ((int)bytes == -1)
+		{
+			free(buffer);
+			return (string);
+		}
+		buffer[bytes] = '\0';
+		string = ft_strjoin(string, buffer);
+	}
+	free(buffer);
+	return (string);
+}
+
+char	*trim_line(char *str)
+{
+	int		len;
+	char	*line;
+
+	len = 0;
+	if (!str[len])
+		return (NULL);
+	while (str[len] && str[len] != '\n')
+		len++;
+	line = (char *) malloc((len + 2) * sizeof(char));
+	if (!line)
+		return (NULL);
+	len = 0;
+	while (str[len] && str[len] != '\n')
+	{
+		line[len] = str[len];
+		len++;
+	}
+	if (str[len] == '\n')
+	{
+		line[len] = '\n';
+		len++;
+	}
+	line[len] = '\0';
+	return (line);
+}
+
+char	*cut_endl(char *string)
+{
+	int		j;
+	int		len;
+	char	*tmp;
+
+	len = 0;
+	while (string[len] && string[len] != '\n')
+		len++;
+	if (!string[len])
+	{
+		free(string);
+		return (NULL);
+	}
+	tmp = (char *)malloc((ft_strlen(string) - len + 1) * sizeof(char));
+	if (!tmp)
+		return (NULL);
+	len++;
+	j = 0;
+	while (string[len])
+		tmp[j++] = string[len++];
+	tmp[j] = '\0';
+	free(string);
+	return (tmp);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*string[4096];
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (NULL);
+	string[fd] = get_line(fd, string[fd]);
+	if (!string[fd])
+		return (NULL);
+	line = trim_line(string[fd]);
+	string[fd] = cut_endl(string[fd]);
+	return (line);
+}
