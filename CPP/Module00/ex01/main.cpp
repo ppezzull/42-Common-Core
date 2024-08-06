c4f3d6c8172fd55c51493b82b2050b3193256a1e
Pietro Jairo Pezzullo <pezzullopietro04@gmail.com>
2024-08-06 05:44:19 +0200

    Module00 ex01 done

--- CPP/Module00/ex01/main.cpp
+++ CPP/Module00/ex01/main.cpp
@@ -10,4 +10,28 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "PhoneBook.hpp"
 
+int main(void) 
+{
+    PhoneBook   phoneBook;
+    std::string command;        
+
+    std::cout << "Ppezzu PhoneBook program\n";
+    while(true)
+    {
+        std::cout << "ADD a contact, SEARCH one or EXIT the program\n";
+        std::cin >> command;
+        if (command == "ADD")
+            phoneBook.add();
+        else if (command == "SEARCH")
+            phoneBook.search();
+        else if (command == "exit")
+            break;
+        else
+            std::cout << "Put a valid command OG\n";
+    }
+
+    return (0);
+}
